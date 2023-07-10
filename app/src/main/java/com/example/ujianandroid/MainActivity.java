4cdcc98452f7a5c74248b41387f804f6e4fda50a
Achmad Misbah <achmadmisbah0013@gmail.com>
2023-07-10 20:47:22 +0700

    UAS menambahkan umur dan status

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -26,34 +26,46 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> daftar_nama = new ArrayList<>();
 
         Intent intent_list = new Intent(MainActivity.this, ListActivity.class);
+
         btnSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
                 String isian_umur = edUmur.getText().toString();
+                Integer doubleisian_nama_umur = Integer.valueOf(isian_umur);
 
-                if(isian_nama_depan.isEmpty()) {
+                if(isian_nama_depan.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
-                } else if (isian_umur.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Umur belum diisi", Toast.LENGTH_SHORT).show();
-                } else {
-                    int jumlah_umur = Integer.parseInt(isian_umur);
+                }else{
                     String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
                     daftar_nama.clear();
 
-                    for (int i = 0; i < jumlah_umur; i++) {
-                        daftar_nama.add(nama_lengkap);
+                    for (int i = 0; i < doubleisian_nama_umur; i++) {
+                        String status;
+//                        pengkondisian umur by achmad
+                        if (doubleisian_nama_umur >= 0 && doubleisian_nama_umur <= 10) {
+                            status = "anak";
+                        } else if (doubleisian_nama_umur >= 11 && doubleisian_nama_umur <= 20) {
+                            status = "remaja";
+                        } else if (doubleisian_nama_umur >= 21 && doubleisian_nama_umur <= 30) {
+                            status = "dewasa";
+                        } else {
+                            status = "tua";
+                        }
+
+                        if (i % 2 == 0) {
+                            // Menambahkan angka incrementing dan status di belakang nama hanya untuk nomor genap oleh achmad
+                            String nama_dengan_status = i + " " + nama_lengkap + ". Status : " + status;
+                            daftar_nama.add(nama_dengan_status);
+                        }
                     }
 
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
+                    edUmur.setText("");
+
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
-
-                    for (String nama : daftar_nama) {
-                        System.out.println(nama);
-                    }
-
                     startActivity(intent_list);
                 }
             }
